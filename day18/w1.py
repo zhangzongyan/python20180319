3255a7321f5b827ca9b8ffbc1f6f62d1d52f7dcb
zhangzongyan <zhangzongyan@uplooking.com>
2018-04-16 17:33:47 +0800

    20180416

--- day18/w1.py
+++ day18/w1.py
@@ -2,21 +2,26 @@
 作者: zhangzongyan
 时间: 18-4-16
 '''
-
+# encoding=utf-8
 import pymysql
 
 # 链接数据库---->register数据库
-mydb = pymysql.connect(host='localhost', user='root', password='123456', db='register', charset='utf8') #port默认值3306
+mydb = pymysql.connect(host='localhost', user='root', password='123456', db='newdb', charset='utf8') #port默认值3306
 # cursor对象
 c = mydb.cursor()
 # 创建用户信息表
-c.execute('''create table if not exists userinfo(no integer primary key, username char(20) not null,
+c.execute('''create table if not exists userinfo(no integer primary key, username nvarchar(128) not null,
         passwd text not null)''')
 
 try:
     c.execute('''insert into userinfo(no, username, passwd) values(1, 'zhangqi', '123456') ''')
     c.execute('''insert into userinfo(no, username, passwd) values(2, 'liqiang', '123456') ''')
 
+except Exception as e:
+    print(e)
+#尝试插入中文
+try:
+    c.execute('''insert into userinfo(no, username, passwd) values(6, '中文2', '123')''')
 except Exception as e:
     print(e)
 c.execute('''select * from userinfo ''')
