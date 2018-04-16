fdd793132ecbe43f9af2c1ee4bde0d6b11ab3542
zhangzongyan <zhangzongyan@uplooking.com>
2018-04-16 12:28:45 +0800

    sql

--- day18/w1.py
+++ day18/w1.py
@@ -2,19 +2,21 @@
 作者: zhangzongyan
 时间: 18-4-16
 '''
+
 import pymysql
 
 # 链接数据库---->register数据库
 mydb = pymysql.connect(host='localhost', user='root', password='123456', db='register', charset='utf8') #port默认值3306
 # cursor对象
 c = mydb.cursor()
-c.execute('''create table if not exists userinfo(no integer primary key , username text not null,
+c.execute('''create table if not exists userinfo(no integer auto_increment primary key, username char(20) not null,
         passwd text not null)''')
-try:
-    c.execute('''insert into userinfo(no, username, passwd) values(2, 'zhangqi', '123456') ''')
-    c.execute('''insert into userinfo(no, username, passwd) values(5, 'liqiang', '123456') ''')
-except :
-    pass
+
+
+c.execute('''insert into userinfo(username, passwd) values('zhangqi', '123456') ''')
+c.execute('''insert into userinfo(username, passwd) values('liqiang', '123456') ''')
+c.execute('''insert into userinfo(username, passwd) values('吴寒', '123456') ''')
+
 mydb.commit()
 mydb.close()
 
