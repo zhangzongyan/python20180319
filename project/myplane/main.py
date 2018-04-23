66187190f9097a90dcd50a5de6d367046beb2a95
zhangzongyan <zhangzongyan@uplooking.com>
2018-04-23 14:04:18 +0800

    new

--- project/myplane/main.py
+++ project/myplane/main.py
@@ -26,6 +26,10 @@ def main():
     # bg图片
     bg_img = pygame.image.load("./images/background.png").convert_alpha()
 
+    # 回去背景的宽度和高度
+    width = bg_img.get_rect().width
+    height = bg_img.get_rect().height
+
     # 标题
     pygame.display.set_caption("飞机大战--Bata")
 
@@ -86,108 +90,135 @@ def main():
     pygame.mixer.music.load("./sound/game_music.ogg")
     pygame.mixer.music.play(-1)
 
+    # 暂停播放按钮
+    pause_nor_img = pygame.image.load("./images/pause_nor.png")
+    pause_pressed_img = pygame.image.load("./images/pause_pressed.png")
+    resume_nor_img = pygame.image.load("./images/resume_nor.png")
+    resume_pressed_img = pygame.image.load("./images/resume_pressed.png")
+    pause_img = pause_nor_img
+    pause_rect = pause_img.get_rect()
+    pause_rect.left, pause_rect.top = (width-pause_rect.width -5, 5)
+    pause_flag = False
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit(1)
-        # 频繁按键
-        pressedkeys = pygame.key.get_pressed()
-        if pressedkeys[K_LEFT]:
-            myplane.move_left()
-        elif pressedkeys[K_RIGHT]:
-            myplane.move_right()
-        elif pressedkeys[K_UP]:
-            myplane.move_up()
-        elif pressedkeys[K_DOWN]:
-            myplane.move_down()
-        # 画背景图片
-        screen.fill((255,255,255))
-        screen.blit(bg_img, bg_img.get_rect())
-
-        # 子弹重置
-        if not delay % 10:
-            bulletlist[bullet_index].reset(myplane.rect.midtop)
-            bullet_index = (bullet_index + 1) % BULLET_NUM
-
-        # 绘制子弹
-        for d in bullet_group:
-            if d.alive:
-                d.move()
-                screen.blit(d.image, d.rect)
-                # 子弹是否与敌机发生碰撞
-                colleny = pygame.sprite.spritecollide(d, enemies_group, False, pygame.sprite.collide_mask)
-                for e in colleny:
-                    if e in smallEny_group:
-                        score += 1000
-                        e.alive = False
+            elif event.type == MOUSEBUTTONDOWN:
+                # 点击鼠标左键 并在按钮区域
+                if event.button == 1 and pause_rect.collidepoint(event.pos):
+                   pause_flag = not pause_flag
+            elif event.type == MOUSEMOTION:
+                if pause_rect.collidepoint(event.pos):
+                    if pause_flag:
+                        # 暂停
+                        pause_img = resume_pressed_img
                     else:
-                        e.energy -= 1
-                        if e.energy == 0:
-                            if e in bigEny_group:
-                                score += 10000
-                            else:
-                                score += 5000
+                        pause_img = pause_pressed_img
+                else:
+                    if pause_flag:
+                        pause_img = resume_nor_img
+                    else:
+                        pause_img = pause_nor_img
+        # 画背景图片
+        screen.fill((255, 255, 255))
+        screen.blit(bg_img, bg_img.get_rect())
+        if not pause_flag:
+            # 频繁按键
+            pressedkeys = pygame.key.get_pressed()
+            if pressedkeys[K_LEFT]:
+                myplane.move_left()
+            elif pressedkeys[K_RIGHT]:
+                myplane.move_right()
+            elif pressedkeys[K_UP]:
+                myplane.move_up()
+            elif pressedkeys[K_DOWN]:
+                myplane.move_down()
+
+            # 子弹重置
+            if not delay % 10:
+                bulletlist[bullet_index].reset(myplane.rect.midtop)
+                bullet_index = (bullet_index + 1) % BULLET_NUM
+
+            # 绘制子弹
+            for d in bullet_group:
+                if d.alive:
+                    d.move()
+                    screen.blit(d.image, d.rect)
+                    # 子弹是否与敌机发生碰撞
+                    colleny = pygame.sprite.spritecollide(d, enemies_group, False, pygame.sprite.collide_mask)
+                    for e in colleny:
+                        if e in smallEny_group:
+                            score += 1000
                             e.alive = False
-                    d.alive = False
+                        else:
+                            e.energy -= 1
+                            if e.energy == 0:
+                                if e in bigEny_group:
+                                    score += 10000
+                                else:
+                                    score += 5000
+                                e.alive = False
+                        d.alive = False
 
-        # 绘制敌机
-        for e in bigEny_group:
-            if e.alive:
-                e.move()
-                if delay % 4 == 0:
-                    screen.blit(e.image1, e.rect)
-                else:
-                    screen.blit(e.image2, e.rect)
-                # 绘制血槽
-                pygame.draw.line(screen, (0,0,0), (e.rect.left, e.rect.top-5), \
-                                 (e.rect.right, e.rect.top-5), 2)
-                # 余血
-                current_egy = e.energy / enemy.BigEnemy.energy
-                if current_egy < 0.2:
-                    color_paint = (255, 0, 0)
-                else:
-                    color_paint = (0, 255, 0)
-                pygame.draw.line(screen, color_paint, (e.rect.left, e.rect.top - 5), \
-                                 (e.rect.left + e.rect.width * current_egy, e.rect.top - 5), 2)
+            # 绘制敌机
+            for e in bigEny_group:
+                if e.alive:
+                    e.move()
+                    if delay % 4 == 0:
+                        screen.blit(e.image1, e.rect)
+                    else:
+                        screen.blit(e.image2, e.rect)
+                    # 绘制血槽
+                    pygame.draw.line(screen, (0,0,0), (e.rect.left, e.rect.top-5), \
+                                     (e.rect.right, e.rect.top-5), 2)
+                    # 余血
+                    current_egy = e.energy / enemy.BigEnemy.energy
+                    if current_egy < 0.2:
+                        color_paint = (255, 0, 0)
+                    else:
+                        color_paint = (0, 255, 0)
+                    pygame.draw.line(screen, color_paint, (e.rect.left, e.rect.top - 5), \
+                                     (e.rect.left + e.rect.width * current_egy, e.rect.top - 5), 2)
 
-            else:
-                screen.blit(e.destroy_image[big_index], e.rect)
-                if delay % 3 == 0:
-                    big_index += 1
-                    if big_index == 6:
-                        big_index = 0
-                        e.reset()
-
-        for e in midEny_group:
-            if e.alive:
-                e.move()
-                screen.blit(e.image, e.rect)
-                # 绘制血槽
-                pygame.draw.line(screen, (0, 0, 0), (e.rect.left, e.rect.top - 5), \
-                                 (e.rect.right, e.rect.top - 5), 2)
-                # 余血
-                current_egy = e.energy / enemy.MidEnemy.energy
-                if current_egy < 0.2:
-                    color_paint = (255,0,0)
                 else:
-                    color_paint = (0,255,0)
-                pygame.draw.line(screen, color_paint, (e.rect.left, e.rect.top - 5), \
-                                 (e.rect.left+e.rect.width * current_egy, e.rect.top - 5), 2)
-            else:
-                screen.blit(e.destroy_image[mid_index], e.rect)
-                if delay % 3 == 0:
-                    mid_index += 1
-                    if mid_index == 4:
-                        mid_index = 0
-                        e.reset()
-        for e in smallEny_group:
-            if e.alive:
-                e.move()
-                screen.blit(e.image, e.rect)
-            else:
-                e.reset()
+                    screen.blit(e.destroy_image[big_index], e.rect)
+                    if delay % 3 == 0:
+                        big_index += 1
+                        if big_index == 6:
+                            big_index = 0
+                            e.reset()
+
+            for e in midEny_group:
+                if e.alive:
+                    e.move()
+                    screen.blit(e.image, e.rect)
+                    # 绘制血槽
+                    pygame.draw.line(screen, (0, 0, 0), (e.rect.left, e.rect.top - 5), \
+                                     (e.rect.right, e.rect.top - 5), 2)
+                    # 余血
+                    current_egy = e.energy / enemy.MidEnemy.energy
+                    if current_egy < 0.2:
+                        color_paint = (255,0,0)
+                    else:
+                        color_paint = (0,255,0)
+                    pygame.draw.line(screen, color_paint, (e.rect.left, e.rect.top - 5), \
+                                     (e.rect.left+e.rect.width * current_egy, e.rect.top - 5), 2)
+                else:
+                    screen.blit(e.destroy_image[mid_index], e.rect)
+                    if delay % 3 == 0:
+                        mid_index += 1
+                        if mid_index == 4:
+                            mid_index = 0
+                            e.reset()
+            for e in smallEny_group:
+                if e.alive:
+                    e.move()
+                    screen.blit(e.image, e.rect)
+                else:
+                    e.reset()
 
         # 检测敌机是否撞击我方飞机
         collide_plane = pygame.sprite.spritecollide(myplane, enemies_group, False, \
@@ -214,6 +245,10 @@ def main():
                 me_index += 1
                 if me_index == 4:
                     running = False
+
+        # 绘制暂停开始
+        screen.blit(pause_img, pause_rect)
+
         delay += 1
         if delay % 5 == 0:
             change_img = True
